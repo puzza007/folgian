c8657e7362941540af7d288644ce588731bb197b
Paul Oliver <puzza007@gmail.com>
2014-10-03 11:19:40 +0200

    Change names

--- src/folgian.erl
+++ src/folgian.erl
@@ -8,7 +8,7 @@ execute(Req, Env) ->
     RawUUID = uuid:get_v4(),
     StringUUID =  uuid:uuid_to_string(RawUUID),
     UUID = list_to_binary(StringUUID),
-    Header = application:get_env(folgian, header, <<"request-id">>),
+    Header = application:get_env(folgian, header, <<"x-folgian-request-id">>),
     ok = add_metadata(folgian_request_id, UUID),
     Req2 = cowboy_req:set_resp_header(Header, UUID, Req),
     {ok, Req2, Env}.
